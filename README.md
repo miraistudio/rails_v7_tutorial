d37a1e83ff4ec7a11542babef18b49e2c9dd6737
h.nomura <>
2023-04-03 15:21:33 +0900

    feat: 細かい設定修正／master.keyをgitignoreから削除

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
 # README
 
 ## 起動方法
+
 ### 初回起動
+
 ```
 make init
 make dbinit
 make up
 ```
 
-### 2回目以降
+### 2 回目以降
+
 ```
 make up
 ```
 
 ## 開発環境アクセス
+
 http://localhost:3010
-
-
-## tailwindを使った開発
-buildをする必要があるので、`make up`とは別のterminalタブで`make dev`を実行しておくことでerbの保存時に必要なtailwindのスタイルが利用可能となる。（buildしないとスタイルが適用されない）
-
-
-## TODO
-- Gem追加時に`make bundle`だけだとgem not foundになることがあり、`make build`が必要になることがある問題を解消
-- フロントと分離できる作りにしているが、本ボイラープレートとしては不要なので削除
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,13 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.paths << Rails.application.config.root.join(
+  "vendor/assets/javascripts",
+)
+Rails.application.config.assets.paths << Rails.application.config.root.join(
+  "vendor/assets/stylesheets",
+)
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
